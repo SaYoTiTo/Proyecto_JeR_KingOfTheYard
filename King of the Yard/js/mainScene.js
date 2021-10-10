32c1ad463ccc91fe7caafefdd3257f3cfc584a66
AlberVicky09 <albervicky09@gmail.com>
2021-10-10 11:25:25 +0200

    Character Attemp #1
    First attemp of character and movement

--- King of the Yard/js/mainScene.js
+++ King of the Yard/js/mainScene.js
@@ -5,6 +5,52 @@ class mainScene extends Phaser.Scene{
     }
 
     create(){
-        console.log("Funsiona");
+        
+        /*this.keys = this.input.keyboard.addKeys(
+            {up:Phaser.Input.Keyboard.KeyCodes.W,
+             down:Phaser.Input.Keyboard.KeyCodes.S,
+             left:Phaser.Input.Keyboard.KeyCodes.A,
+             right:Phaser.Input.Keyboard.KeyCodes.D,
+             up2:Phaser.Input.Keyboard.KeyCodes.I,
+             down2:Phaser.Input.Keyboard.KeyCodes.K,
+             left2:Phaser.Input.Keyboard.KeyCodes.J,
+             right2:Phaser.Input.Keyboard.KeyCodes.L,
+             space2:Phaser.Input.Keyboard.KeyCodes.P}
+        );*/           
+            
+        this.input.mouse.disableContextMenu();
+
+        this.add.sprite(210, 160, 'menuBg', 0);
+
+        this.player = this.add.rectangle(210, 160, 30, 30, 0x6666ff);
+
+        this.physics.add.existing(this.player);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+    }
+
+    update(){
+        if (this.cursors.left.isDown)
+        {
+            this.player.body.velocity.x = -160;
+            this.player.body.velocity.y = 0;
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.body.velocity.x = 160;
+            this.player.body.velocity.y = -0;
+        }
+        else if (this.cursors.up.isDown){
+            this.player.body.velocity.x = 0;
+            this.player.body.velocity.y = -160;
+        }
+        else if (this.cursors.down.isDown){
+            this.player.body.velocity.x = 0;
+            this.player.body.velocity.y = 160;
+        }else{
+            this.player.body.velocity.x = 0;
+            this.player.body.velocity.y = 0;
+        }
     }
 }
--- King of the Yard/js/preloadScene.js
+++ King of the Yard/js/preloadScene.js
@@ -6,9 +6,46 @@ class preloadScene extends Phaser.Scene{
 
     preload(){
 
+        //Load Bar drawing
+        this.graphics = this.add.graphics();
+        this.newGraphics = this.add.graphics();
+        var progressBar = new Phaser.Geom.Rectangle(100, 100, 200, 25);
+        var progressBarFill = new Phaser.Geom.Rectangle(102, 102, 190, 15);
+
+        this.graphics.fillStyle(0xffffff, 1);
+        this.graphics.fillRectShape(progressBar);
+
+        this.newGraphics.fillStyle(0x3587e2, 1);
+        this.newGraphics.fillRectShape(progressBarFill);
+
+        var loadingText = this.add.text(65,150,"Loading: ", { fontSize: '32px', fill: '#FFF' });
+
+        //Slider
+        var url;
+        url = 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexsliderplugin.min.js';
+        this.load.plugin('rexsliderplugin', url, true);
+        
+        //Barra de carga
+        this.load.on('progress', this.updateBar, {newGraphics:this.newGraphics,loadingText:loadingText});
+        this.load.on('complete', this.complete, {scene: this.scene});
+
+        //Fondo
+        this.load.image('menuBg', 'assets/fondoPrueba.png');
     }
 
-    create(){
-        console.log("Funsiona");
+    updateBar(percentage) {
+        this.newGraphics.clear();
+        this.newGraphics.fillStyle(0x3587e2, 1);
+        this.newGraphics.fillRectShape(new Phaser.Geom.Rectangle(102, 102, percentage*190, 20));
+            
+        percentage = percentage * 100;
+        this.loadingText.setText("Loading: " + percentage.toFixed(2) + "%");
+        console.log("P:" + percentage); 
+    }
+
+    complete() {
+	    console.log("COMPLETE!");
+        game.scene.stop('preloadScene');
+        game.scene.start('mainScene');        
     }
 }
