1c84cceed62345f6125bc20e398a68ef78a7dc7b
strikealex <43806315+strikealex@users.noreply.github.com>
2021-11-18 13:01:42 +0100

    Botones de play again en pantallas de victoria

--- King of the Yard/js/redWinScene.js
+++ King of the Yard/js/redWinScene.js
@@ -18,9 +18,11 @@ class redWinScene extends Phaser.Scene{
         this.bg = this.add.sprite(600, 337.5, 'victoryBg');
         this.bg.anims.play('victoryBgAnim');
 
+        this.play = this.physics.add.sprite(575, 400, 'playButton').setScale(0.8).refreshBody();
         this.redName = this.add.sprite(600, 300, 'redName');
         this.red = this.add.sprite(600, 500, 'redChar');
 
+        this.play.setInteractive();
         this.bg.setInteractive();
 
         this.enterKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
@@ -40,6 +42,18 @@ class redWinScene extends Phaser.Scene{
             victoryBgMusic.setVolume(0);
             this.scene.scene.stop('redWinScene');
             this.scene.scene.start('mainMenu');
+        });
+        this.play.on("pointerover", () => {
+            this.alpha = 0;
+            this.playPressed.alpha = 1;
+        });
+        this.play.on("pointerout", () => {
+            this.alpha = 1;
+            this.playPressed.alpha = 0;
+        });
+        this.play.on("pointerdown", function(){
+            this.scene.scene.stop('mainMenu');
+            this.scene.scene.start('selectionMenu');
         });
     }
 }
